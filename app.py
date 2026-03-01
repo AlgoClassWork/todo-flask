950bd718533b682611c861f34db56c79c2a20540
AlgoClassWork <kyxec.play@gmail.com>
2026-03-01 11:02:05 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
-from queries import get_all_todos, add_todo
+from queries import get_all_todos, add_todo, delete_todo
 
 app = Flask(__name__)
 
-#http://127.0.0.1:5000/ 
+#http://127.0.0.1:5000/
 @app.route('/') 
 def index():
     todos = get_all_todos()
@@ -15,6 +15,12 @@ def add():
     title = request.form['title']
     if title:
         add_todo(title)
+    return redirect( url_for('index') )
+
+#http://127.0.0.1:5000/delete/int
+@app.route('/delete/<int:id>') 
+def delete(id):
+    delete_todo( id )
     return redirect( url_for('index') )
     
 app.run(debug=True)
